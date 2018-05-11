f5573d3f37220ffa3ebfe8633c08a17dac6239a2
Don Le <donle@kainos.com>
2018-05-11 14:19:16 +0100

    logic for having images on the cards instead of numbers

--- resources/js/match-game.js
+++ resources/js/match-game.js
@@ -1,101 +1,79 @@
 var MatchGame = {};
-
 /*
   Sets up a new game after HTML document has loaded.
   Renders a 4x4 board of cards.
 */
-
 $(document).ready(function() {
   var $game = $('#game');
   var values = MatchGame.generateCardValues();
   MatchGame.renderCards(values, $game);
 });
-
 /*
   Generates and returns an array of matching card values.
  */
-
 MatchGame.generateCardValues = function () {
  var numbers = [];
-
  for (var i = 1; i <= 8; i++) {
    numbers.push(i);
    numbers.push(i);
  }
-
 var cards = [];
-
 while (numbers.length > 0) {
   var randomIndex = Math.floor(Math.random() * numbers.length);
   var randomNumber = numbers.splice(randomIndex, 1)[0];
   cards.push(randomNumber);
 }
-
 return cards;
-
 };
-
 /*
   Converts card values to jQuery card objects and adds them to the supplied game
   object.
 */
-
 MatchGame.renderCards = function(cards, $game) {
-  var colors = [
-    'hsl(25, 85%, 65%)',
-    'hsl(55, 85%, 65%)',
-    'hsl(90, 85%, 65%)',
-    'hsl(160, 85%, 65%)',
-    'hsl(220, 85%, 65%)',
-    'hsl(265, 85%, 65%)',
-    'hsl(310, 85%, 65%)',
-    'hsl(360, 85%, 65%)'];
-
+  var images = [
+    'url(\'resources/images/Kainos.png\')',
+    'url(\'resources/images/Kainos.png\')',
+    'url(\'resources/images/Kainos.png\')',
+    'url(\'resources/images/Kainos.png\')',
+    'url(\'resources/images/Kainos.png\')',
+    'url(\'resources/images/Kainos.png\')',
+    'url(\'resources/images/Kainos.png\')',
+    'url(\'resources/images/Kainos.png\')'];
     $game.empty();
     $game.data('flippedCards', []);
-
   for (var j = 0; j < cards.length; j++) {
     var value = cards[j];
-    var color = colors[value - 1];
+    var image = images[value - 1];
     var data = {
       value: value,
-      color: color,
+      image: image,
       isFlipped: false
   };
-
 var $cardElement = $('<div class="col-xs-3 card"></div>');
-
 $cardElement.data(data);
 $game.append($cardElement);
 }
-
 $('.card').click(function() {
   MatchGame.flipCard($(this), $('#game'));
 });
-
 };
-
 /*
   Flips over a given card and checks to see if two cards are flipped over.
   Updates styles on flipped cards depending whether they are a match or not.
  */
-
 MatchGame.flipCard = function($card, $game) {
   if ($card.data('isFlipped')) {
     return;
   }
-
-  $card.css('background-color', $card.data('color'))
-      .text($card.data('value'))
+  $card.css('background-image', $card.data('image'))
       .data('isFlipped', true);
-
   var flippedCards = $game.data('flippedCards');
   flippedCards.push($card);
-
   if (flippedCards.length === 2) {
     if (flippedCards[0].data('value') === flippedCards[1].data('value')) {
       var matchCss = {
         backgroundColor: 'rgb(153, 153, 153)',
+        backgroundImage: 'none',
         color: 'rgb(204, 204, 204)'
       };
       flippedCards[0].css(matchCss);
@@ -103,12 +81,14 @@ MatchGame.flipCard = function($card, $game) {
     } else {
       var card1 = flippedCards[0];
       var card2 = flippedCards[1];
+      var css = {
+        backgroundColor: 'rgb(32, 64, 86)',
+        backgroundImage: 'none'
+      }
       window.setTimeout(function() {
-        card1.css('background-color', 'rgb(32, 64, 86)')
-            .text('')
+        card1.css(css)
             .data('isFlipped', false);
-        card2.css('background-color', 'rgb(32, 64, 86)')
-            .text('')
+        card2.css(css)
             .data('isFlipped', false);
       }, 350);
     }
